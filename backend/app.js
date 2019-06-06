7ee997f8524da780fc05f21a4e12ecf54bb5a98a
stage <stage>
2019-06-06 09:56:18 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const postsRoutes = require('./routes/posts')
+const app = express();
+
+mongoose.connect("mongodb+srv://kechmech:pluGO0MbbxzPAsKe@cluster0-yjjrm.mongodb.net/mean?retryWrites=true&w=majority", { useNewUrlParser: true })
+  .then(() =>{
+    console.log('Connected to database!')
+  })
+  .catch(() => {
+    console.log('Failed to connect to database!')
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
+//   pluGO0MbbxzPAsKe
+
+app.use('/api/posts', postsRoutes);
+
+module.exports = app;
